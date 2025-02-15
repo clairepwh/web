961229b06bd7eca58e0c21028ba9c6d7dc6f2df5
tianshin32 <d11332236@gapps.fg.tp.edu.tw>
2025-02-15 22:26:00 +0800

    v.0.0.7

--- js.js
+++ js.js
@@ -1,1 +1,15 @@
-~~
+function kind(number)
+{
+let a = number;
+
+switch(a){
+    case "Chinese":window.location.href = './Chinese style.html';
+        break;
+    case "Japanese":window.location.href = './Japanese style.html';
+        break;
+    case "Korean":window.location.href = './Korean style.html';
+        break;
+    case "Western":window.location.href = './Western.html';
+        break;
+}
+}
